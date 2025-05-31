211887bde4f029e5ef3d032fed24e6738eeed231
md.munna <mk8761174@gmail.com>
2025-05-31 12:06:19 +0600

    13-7 Routing in node js

--- todo-App/server.js
+++ todo-App/server.js
@@ -2,10 +2,20 @@ const http = require("http")
 
 
 const server=http.createServer((req,res)=>{
-    console.log({req,res})
-    res.end("welcome to todo app server")
+   if(req.url === "/todos" && req.method ==="GET"){
+    res.end("ALL Todos")
+   }else if(req.url === "/todos/create-todo" && req.method ==="POST"){
+    res.end("Todo Created")
+   } else{
+    res.end("404")
+   }
 })
 
 server.listen(5000,"127.0.0.1",()=>{
     console.log("server listening to port 5000")
 })
+/**\
+ * /todos Get -all todo
+ * 
+ * /todos /create todos post create
+ */
